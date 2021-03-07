f1d79b8682fba131b6df341c7935bf1d7d33c97a
Martin Gejer <mgejer123@gmail.com>
2021-03-07 17:06:39 +0000

    aliases

---
+++ aliases.sh
@@ -0,0 +1,6 @@
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.cm commit
+git config --global alias.stt status
+git config --global alias.ps push
+git config --global alias.pll pull
