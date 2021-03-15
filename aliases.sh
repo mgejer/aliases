6763c5dd684bf6f3ceb9450c4c897ef5ea20ccfa
Martin Gejer <mgejer123@gmail.com>
2021-03-15 16:37:07 +0000

    Dad alias!

--- aliases.sh
+++ aliases.sh
@@ -4,3 +4,6 @@ git config --global alias.cm commit
 git config --global alias.stt status
 git config --global alias.ps push
 git config --global alias.pll pull
+git config --global alias.rst reset
+git config --global alias.cl clone
+git config --global alias.dad '!curl https://icanhazdadjoke.com/ && echo'
